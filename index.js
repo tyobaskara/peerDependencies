be4b8a11800b5844bc08da0e18e76b8c1cf5f966
Prasetya Aji Baskara <prasetya.baskara@btpn.com>
2021-07-15 10:23:33 +0700

    remove sec lib

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var _ = require('lodash');
-// var lodashGet = require('secondLibrary');
+// var secondLibrary = require('secondLibrary');
 
 const myData = {
   name: 'Prasetya'
@@ -8,8 +8,9 @@ const myData = {
 const callFunction = () => {
   const myName = _.get(myData, 'name', 'Anonymous');
   console.log('first func', myName);
+
   // console.log('second func');
-  // lodashGet();
+  // secondLibrary.callFunction();
 };
 
 module.exports = {
